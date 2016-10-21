6962d15f50c97cec61fe450adbbad3f84a3621cc
dailong <dailong@bonc.com.cn>
2016-10-21 17:29:00 +0800

    ddd

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import service.impl.HelloWord;
+
+import java.util.Date;
+
+/**
+ * Created by dailong on 2016/3/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Date date = new Date();
+
+
+        System.out.println("aa");
+        String a = "afddsa";
+        String c = "afddsa";
+        String d = "afddsa";
+
+
+       /* HelloWord hw = new HelloWord();
+        hw.getHelloWordImplPort().sayHello();*/
+
+
+    }
+}
