63ad1ce3b55cc5558bb6f784a849c24e8f9f11b3
dailong <692444155@qq.com>
2016-11-01 14:26:14 +0800

    fetch

--- src/UploadServlet.java
+++ src/UploadServlet.java
@@ -19,7 +19,7 @@ import javax.servlet.http.Part;
 public class UploadServlet extends HttpServlet {
 
     /**
-     add12
+     add12fetch
      **/
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
