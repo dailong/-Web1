c910b3dd89ce925c519d73c48b2a22d17129d21e
dailong <dailong@bonc.com.cn>
2016-11-01 14:24:15 +0800

    add2

--- src/UploadServlet.java
+++ src/UploadServlet.java
@@ -19,7 +19,7 @@ import javax.servlet.http.Part;
 public class UploadServlet extends HttpServlet {
 
     /**
-     add1
+     add12
      **/
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
