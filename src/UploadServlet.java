5182f9be7ad0d06ae17f716e4e948054a35e50b1
dailong <692444155@qq.com>
2016-11-01 14:13:55 +0800

    Update UploadServlet.java

--- src/UploadServlet.java
+++ src/UploadServlet.java
@@ -19,7 +19,7 @@ import javax.servlet.http.Part;
 public class UploadServlet extends HttpServlet {
 
     /**
-     add122422
+     add
      **/
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
